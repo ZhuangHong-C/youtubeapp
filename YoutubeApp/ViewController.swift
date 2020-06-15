a126a9f7e1c9f8f0905d1847fe38dbee09740fc1
Chong Zhuang Hong <zhuanghong@Chongs-MacBook-Air.local>
2020-06-15 18:51:11 +0800

    Initial Xcode Project Creation

---
+++ YoutubeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubeApp
+//
+//  Created by Chong Zhuang Hong on 15/06/2020.
+//  Copyright © 2020 Chong Zhuang Hong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
